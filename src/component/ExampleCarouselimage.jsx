aa2e68a98011a22c1e93e4416076947d80a62766
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2023-11-23 02:30:11 +0300

    in progress

---
+++ src/component/ExampleCarouselimage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleCarouselImage = ({ text }) => {
+  return (
+    <div>
+      <img src="img/salo1.webp" alt="Carousel Image" />
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default ExampleCarouselImage;
