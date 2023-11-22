92cc8d87be573afb56a44460780aced64fd2179e
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2023-11-22 20:22:42 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./component/navbar"; // Assuming you have a Navbar component
-
+import Navbar from "./component/navbar";
 import Home from "./component/home";
-import About from "./component/about";
-import Contact from "./component/contact";
+import About from './component/aboutUs'; // Düzeltildi
+import Details from "./component/details";
+import Contact from "./component/contacksAndReservations";
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import "./App.css";
 
-class App extends Component  {
+class App extends Component {
     render() {
         return (
             <Router>
@@ -15,9 +17,10 @@ class App extends Component  {
                     <Navbar />
                     {/* Routes içinde Route'ları kullanarak rotaları belirtin */}
                     <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/details" element={<Details />} />
+                        <Route path="/aboutUs" element={<About />} /> {/* Düzeltildi */}
+                        <Route path="/contacksAndReservations" element={<Contact />} />
                     </Routes>
                 </div>
             </Router>
