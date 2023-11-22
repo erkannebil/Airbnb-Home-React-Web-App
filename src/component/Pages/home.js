b016150381bd603a4ea01cd1eefbd94f8f8a8cf7
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2023-11-23 02:36:57 +0300

    in progress

--- src/component/Pages/home.js
+++ src/component/Pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CarouselSlides from "../Carousel"
+
 const features = [
   { name: 'Göl erişimi', description: 'Evinizde özel bir göl erişimi keyfi yaşayın. Doğanın güzellikleriyle çevrili, huzur dolu anlar için ideal bir ortam.' },
   { name: 'Wifi', description: 'Wi-Fi ile bağlantınızı kesintisiz sürdürün' },
@@ -69,49 +69,9 @@ const Home = () => {
 
 
      <section>
-     <CarouselSlides/>
+    
      </section>
 
-      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-        <div>
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
-          <p className="mt-4 text-gray-500">
-            The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder-coated
-            steel divider separates active cards from new ones, or can be used to archive important task lists.
-          </p>
-
-          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-            {features.map((feature) => (
-              <div key={feature.name} className="border-t border-gray-200 pt-4">
-                <dt className="font-medium text-gray-900">{feature.name}</dt>
-                <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
-              </div>
-            ))}
-          </dl>
-        </div>
-        <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
-            alt=""
-            className="rounded-lg bg-gray-100"
-          />
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
-            alt=""
-            className="rounded-lg bg-gray-100"
-          />
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
-            alt=""
-            className="rounded-lg bg-gray-100"
-          />
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
-            alt=""
-            className="rounded-lg bg-gray-100"
-          />
-        </div>
-      </div>
     </div>
   );
 };
