56e91a328f84d6ab15ae0f3365b502209c453c81
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2023-11-22 16:17:51 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/navbar"; // Assuming you have a Navbar component
+
+import Home from "./component/home";
+import About from "./component/about";
+import Contact from "./component/contact";
+import "./App.css";
+
+class App extends Component  {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    {/* Routes içinde Route'ları kullanarak rotaları belirtin */}
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
---
+++ src/component/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link,NavLink,withRouter}from 'react-router-dom'
+const Navbar = () => {
+return (
+<nav>
+<div class="nav-wrapper">
+<a href="#" class="brand-logo">Logo</a>
+<ul id="nav-mobile" class="right hide-on-med-and-down">
+<li><Link to="/">Home</Link></li>
+<li><Link to="/about">About</Link></li>
+<li><Link to="/contact">Contact</Link></li>
+</ul>
+</div>
+</nav>
+)
+}
+export default Navbar;
